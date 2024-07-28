b8b5a9a0d1f8465ffa2280914150c1599a95e3f9
rakibulmuhajir <escapefromcruelworld@gmail.com>
2024-07-28 19:19:43 +0500

    Update chat.js

--- static/js/components/chat.js
+++ static/js/components/chat.js
@@ -305,7 +305,7 @@ function actionTrigger() {
 // eslint-disable-next-line no-unused-vars
 function customActionTrigger() {
   $.ajax({
-    url: "http://localhost:5055/webhook/",
+    url: "https://aivolutive.com:5055/webhook/",
     type: "POST",
     contentType: "application/json",
     data: JSON.stringify({
