327dc0cb9d6506ea21fcce64cfce8fecc7fe0d6a
rakibulmuhajir <escapefromcruelworld@gmail.com>
2024-07-29 22:42:12 +0500

    Update widget-init.js

--- static/js/widget-init.js
+++ static/js/widget-init.js
@@ -28,7 +28,7 @@ const chatWidgetHTML = `
       <div class="chat_header">
         <span class="chat_header_title">SecPro Bot</span>
         <span class="dropdown-trigger" href="#" data-target="dropdown1">
-          <i class="material-icons"> more_vert </i>
+          <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 128 512"><!--!Font Awesome Free 6.6.0 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license/free Copyright 2024 Fonticons, Inc.--><path d="M64 360a56 56 0 1 0 0 112 56 56 0 1 0 0-112zm0-160a56 56 0 1 0 0 112 56 56 0 1 0 0-112zM120 96A56 56 0 1 0 8 96a56 56 0 1 0 112 0z"/></svg>
         </span>
         <ul id="dropdown1" class="dropdown-content">
           <li><a href="#" id="clear">Clear</a></li>
