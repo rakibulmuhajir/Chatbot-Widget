12a43b1195b7df0dd49db4c1381a809cdd1d3a3d
rakibulmuhajir <escapefromcruelworld@gmail.com>
2024-08-13 13:17:38 +0500

    Update botTyping.js

--- static/js/components/botTyping.js
+++ static/js/components/botTyping.js
@@ -11,7 +11,7 @@ function hideBotTyping() {
  * adds the bot typing indicator from the chat screen
  */
 function showBotTyping() {
-    const botTyping = '<img class="botAvatar" id="botAvatar" src="https://bot.aivolutive.com/static/img/sara_avatar.png"/><div class="botTyping"><div class="bounce1"></div><div class="bounce2"></div><div class="bounce3"></div></div>';
+    const botTyping = '<img class="botAvatar" id="botAvatar" src="https://cdn.shopify.com/s/files/1/0003/4786/5124/files/Security-pro-logo.png?v=1723305472"/><div class="botTyping"><div class="bounce1"></div><div class="bounce2"></div><div class="bounce3"></div></div>';
     $(botTyping).appendTo(".chats");
     $(".botTyping").show();
     scrollToBottomOfResults();
