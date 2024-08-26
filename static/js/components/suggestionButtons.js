30595ed8a98ef24738c6247e599a6263bd39b596
rakibulmuhajir <escapefromcruelworld@gmail.com>
2024-08-26 06:18:19 +0500

    Update suggestionButtons.js

--- static/js/components/suggestionButtons.js
+++ static/js/components/suggestionButtons.js
@@ -13,23 +13,32 @@ function addSuggestion(suggestions) {
             .fadeIn(1000);
         // Loop through suggestions
         for (let i = 0; i < suggLength; i += 1) {
+            const isAddToCart = suggestions[i].payload && suggestions[i].payload.startsWith('/add_to_cart');
+            const buttonClass = isAddToCart ? 'menuChips add-to-cart-button' : 'menuChips';
+            const variantId = isAddToCart ? suggestions[i].payload.split('=')[1] : '';
             $(
-                `<div class="menuChips" data-payload='${suggestions[i].payload}'>${suggestions[i].title}</div>`,
+                `<div class="${buttonClass}" data-payload='${suggestions[i].payload}' ${isAddToCart ? `data-variant-id="${variantId}"` : ''}>${suggestions[i].title}</div>`,
             ).appendTo(".menu");
         }
         scrollToBottomOfResults();
     }, 1000);
 }
 
-
 // on click of suggestion's button, get the title value and send it to rasa
-$(document).on("click", ".menu .menuChips", function () {
+$(document).on("click", ".menu .menuChips", function (e) {
     const text = this.innerText;
     const payload = this.getAttribute("data-payload");
-    console.log("payload: ", this.getAttribute("data-payload"));
-    setUserResponse(text);
-    send(payload);
-
+    console.log("payload: ", payload);
+    
+    if ($(this).hasClass('add-to-cart-button')) {
+        e.preventDefault();
+        const variantId = this.getAttribute("data-variant-id");
+        handleAddToCart(variantId, 1);
+    } else {
+        setUserResponse(text);
+        send(payload);
+    }
+    
     // delete the suggestions once user click on it.
     $(".suggestions").remove();
 });
